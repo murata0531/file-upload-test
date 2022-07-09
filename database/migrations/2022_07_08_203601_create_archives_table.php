e87b791afa7e38ad7f8e9f58ebbf26228f097a30
murata0531 <coless115@gmail.com>
2022-07-09 13:58:18 +0900

    アーカイブデータ

---
+++ database/migrations/2022_07_08_203601_create_archives_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('archives', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('organization', 50);
+            $table->unsignedBigInteger('category_id');
+            $table->string('img_url');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('archives');
+    }
+};
