db06c864cbce4aa255ea4b97ff69a8aecd3d5349
murata0531 <coless115@gmail.com>
2022-07-09 14:00:01 +0900

    カテゴリテーブルのシーダー

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                'id' => 1,
+                'name' => 'develop'
+            ],
+            [
+                'id' => 2,
+                'name' => 'test'
+            ],
+            [
+                'id' => 3,
+                'name' => 'production'
+            ],
+        ]);
+    }
+}
